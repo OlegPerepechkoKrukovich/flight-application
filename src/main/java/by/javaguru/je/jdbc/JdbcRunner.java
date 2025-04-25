49388cccb0d54f40ef81f18513673445a62b6682
PKO <PKO@gmail.com>
2025-04-25 11:46:56 +0300

    second commit (jdbc part 3, DAO)

--- src/main/java/by/javaguru/je/jdbc/JdbcRunner.java
+++ src/main/java/by/javaguru/je/jdbc/JdbcRunner.java
@@ -1,24 +1,19 @@
 package by.javaguru.je.jdbc;
 
-import by.javaguru.je.jdbc.utils.ConnectionManager;
+import by.javaguru.je.jdbc.dao.FlightDao;
+import by.javaguru.je.jdbc.dao.TicketDao;
+import by.javaguru.je.jdbc.dto.TicketFilter;
 
 
-import java.sql.SQLException;
 
 public class JdbcRunner {
-    public static void main(String[] args) throws SQLException {
-        String sql = """
-                SELECT * FROM ticket;
-                """;
-        try (var connection = ConnectionManager.get(); var statement = connection.createStatement()) {
-//            System.out.println(statement.executeUpdate(sql));
-              var result = statement.executeQuery(sql);
-              while (result.next()) {
-                  System.out.println(result.getLong("id"));
-                  System.out.println(result.getString("passenger_name"));
-                  System.out.println("-----------------");
-              }
-        }
+    public static void main(String[] args)  {
+        var ticketDao = TicketDao.getInstance();
+        var filter = new TicketFilter(null, null, 5, 0);
+        System.out.println(ticketDao.findALl(filter));
+        System.out.println("__________________");
+        var flightDao = FlightDao.getInstance();
+        System.out.println(flightDao.findALl());
 
 
     }
